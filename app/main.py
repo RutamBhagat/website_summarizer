6c62d21384eb0a64185a4f31e6bfe4045cf3675e
RutamBhagat <rutambhagat@gmail.com>
2024-10-24 19:00:13 -0700

    feat(api): cors update

--- app/main.py
+++ app/main.py
@@ -34,6 +34,8 @@ if settings.all_cors_origins:
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
+        expose_headers=["*"],  # Add this
+        max_age=3600,  # Add this
     )
 
 
